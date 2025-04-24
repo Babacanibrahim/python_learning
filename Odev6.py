66c5b275282e03ce65b571652c021bdabc62aa0c
Babacanİbrahim <babacan-1907@outlook.com.tr>
2025-04-25 02:27:26 +0300

    Dosya isimleri aciklayici sekilde degistirilir

---
+++ Odev6.py
@@ -0,0 +1,9 @@
+# İsim ve soyisimleri birleştirip alfabetik şekilde yazdırma
+isim=["Kerim","Tarık","Ezgi","Kemal","İlkay","Şükran","Merve"]
+
+soyisim=["Yılmaz","Öztürk","Dağdeviren","Atatürk","Dikmen","Kaya","Polat"]
+
+liste=list(zip(isim,soyisim))
+liste.sort()
+for i,j in liste:
+    print(i,j)
