de13cd673c77978ff1d20e8f5214edbbbb5322a3
Babacanİbrahim <babacan-1907@outlook.com.tr>
2025-07-18 16:36:43 +0300

    Django ile makale uygulamasina devam edilir.

---
+++ django-learning/blog/user/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from . import forms
+
+# Create your views here.
+
+def register(request):
+    form = forms.RegisterForm()
+    context = {
+        "form" : form
+    }
+
+    return render(request,"register.html" , context)
+
+def login(request):
+    return render(request,"login.html")
+
+
+def logout(request):
+    pass
